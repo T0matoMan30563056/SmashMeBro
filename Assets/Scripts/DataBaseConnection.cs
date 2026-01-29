fdc7b0c68af93ceb45a9f0e1fa65caeec62ff66a
MatejMikic3056 <125775808+T0matoMan30563056@users.noreply.github.com>
2026-01-29 14:15:23 +0100

    added sign up, and kinda got log inn
    finished up and completed the register menu as well as we got login to kinda work

--- Assets/Scripts/DataBaseConnection.cs
+++ Assets/Scripts/DataBaseConnection.cs
@@ -2,9 +2,23 @@ using UnityEngine;
 using UnityEngine.Networking;
 using System.Collections;
 using System.Text;
+using Unity.VisualScripting;
+using System;
 
 public class DataBaseConnection : MonoBehaviour
 {
+
+    public static DataBaseConnection instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+
     public IEnumerator Login(string username, string password)
     {
         string json = JsonUtility.ToJson(new LoginData
@@ -34,6 +48,38 @@ public class DataBaseConnection : MonoBehaviour
             Debug.Log("Response: " + request.downloadHandler.text);
         }
     }
+
+    public IEnumerator SignIn(string username, string password)
+    {
+        string json = JsonUtility.ToJson(new LoginData
+        {
+            username = username,
+            password = password
+        });
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(json);
+
+
+        UnityWebRequest request = new UnityWebRequest(
+            "http://10.200.14.25:5000/SignUp",
+            "POST"
+        );
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+
+
+        if (request.result != UnityWebRequest.Result.Success)
+        {
+            Debug.LogError(request.error);
+        }
+        else
+        {
+            Debug.Log("Response: " + request.downloadHandler.text);
+        }
+    }
+
+
     public IEnumerator TestRequest()
     {
         UnityWebRequest request = new UnityWebRequest(
@@ -61,10 +107,6 @@ public class DataBaseConnection : MonoBehaviour
         public string username;
         public string password;
     }
-    private void Start()
-    {
-        StartCoroutine(Login("t0mato", "NeGeR"));
-        StartCoroutine(TestRequest());
-    }
+
 
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,22 +1,46 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
-    
+
+    [SerializeField] TMP_InputField usernameField;
+    [SerializeField] TMP_InputField passwordField;
     public void Startgame()
     {
         SceneManager.LoadSceneAsync(1);
     }
 
-    public void LogIn()
+    public void LoggInTransfer()
     {
         SceneManager.LoadSceneAsync(2);
     }
+    public void LoggIn()
+    {
+        string username = usernameField.text;
+        string password = passwordField.text;
+
+        StartCoroutine(DataBaseConnection.instance.Login(username, password));
+        Debug.Log(username + " " + password);
+    }
+
+    public void SignUpTransfer()
+    {
+        SceneManager.LoadSceneAsync(3);
+    }
+
 
     public void SignUp()
     {
-        SceneManager.LoadSceneAsync(3);
+        string username = usernameField.text;
+        string password = passwordField.text;
+
+        StartCoroutine(DataBaseConnection.instance.SignIn(username, password));
+        Debug.Log(username + " " + password);
     }
 
 }
